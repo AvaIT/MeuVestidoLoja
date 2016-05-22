9eba372cd31f9e6162c1b356b22b71defea98d5a
clariessss <clariessss@gmail.com>
2016-05-22 14:58:21 -0300

    commit repo

---
+++ MeuVestido.LojaVirtual.Dominio/Repositorio/EfDbContex.cs
@@ -0,0 +1,20 @@
+﻿using MeuVestido.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuVestido.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContex : DbContex
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuider)
+        {
+            modelBuider.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuider.Entity<Produto>().ToTable("Produtos");        
+        }
+
+}
---
+++ MeuVestido.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using MeuVestido.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuVestido.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EfDbContex _context()=new EfDbContex();
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+    }
+}
