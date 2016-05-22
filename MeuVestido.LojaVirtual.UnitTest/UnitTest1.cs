1bb449b8fcc245d3a0468bbe35c8758e6f19f3f3
clariessss <clariessss@gmail.com>
2016-05-22 02:13:13 -0300

    initial comit

---
+++ MeuVestido.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MeuVestido.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestMeuVestido
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ MeuVestido.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeuVestido.LojaVirtual.Web.MvcApplication" Language="C#" %>
